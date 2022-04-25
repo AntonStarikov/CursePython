97aff0a9851a78126d34df08b8d32177e75fd1d2
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-04-25 16:34:36 +0300

    Vebinar

--- HomeWoorkSkillFacktory.py
+++ HomeWoorkSkillFacktory.py
@@ -1,3 +1,16 @@
 f = open("Test.txt", 'r', -1, 'utf-8')
-print(f.read())
+text = f.readline()
+f.close()
+
+count = 0
+for i in text:
+    a = i.split()
+    count += len(a)
+    print(count)
+f = open("Test.txt", 'a', -1, 'utf-8')
+f.write('В тексте'+ str(count) +'слов.')
+f.close()
+
+f = open("Test.txt", 'r', -1, 'utf-8')
+f.read()
 f.close()
