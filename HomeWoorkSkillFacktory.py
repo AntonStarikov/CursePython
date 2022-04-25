83d58755af958f6c73913a501fb04e04d7f3e817
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-04-25 15:34:40 +0300

    n14.2

--- HomeWoorkSkillFacktory.py
+++ HomeWoorkSkillFacktory.py
@@ -1,13 +1,3 @@
-def filter(func, cont):
-    outp = []
-    for x in cont: # проходим циклом по итерируемому объекту
-        if func(x): # проверяем условие для каждого элемента
-            outp.append(x) # если True, добавляем в новый список
-    return outp
-def positive(x):
-    return x %2 == 0  #
-
-result = filter(positive, [-2, -1, 0, 1, -3, 2, -3])
-
-# Возвращается итератор, т.е. перечисляйте или приводите к списку
-print(list(result))   # [1, 2]
+f = open("Test.txt", 'r', -1, 'utf-8')
+print(f.read())
+f.close()
