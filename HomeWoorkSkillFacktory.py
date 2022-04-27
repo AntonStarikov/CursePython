ddedec9208b8836d4120db9ad6675c18712cb5b1
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-04-27 14:46:37 +0300

    Работа с файлами

--- HomeWoorkSkillFacktory.py
+++ HomeWoorkSkillFacktory.py
@@ -1,16 +1,23 @@
-f = open("Test.txt", 'r', -1, 'utf-8')
-text = f.readline()
-f.close()
+alpha = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+alphaUp = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+number = int(input('Введите число, на которое нужно сдвинуть текст: '))
 
-count = 0
-for i in text:
-    a = i.split()
-    count += len(a)
-    print(count)
-f = open("Test.txt", 'a', -1, 'utf-8')
-f.write('В тексте'+ str(count) +'слов.')
-f.close()
+summary = ''
 
-f = open("Test.txt", 'r', -1, 'utf-8')
-f.read()
-f.close()
+
+def changeChar(char):
+    if char in alpha:
+        return alpha[(alpha.index(char) + number) % len(alpha)]
+    elif char in alphaUp:
+        return alphaUp[(alphaUp.index(char) + number) % len(alphaUp)]
+    else:
+        return char
+
+
+with open("filename2.txt", encoding="utf8") as myFile:
+    for line in myFile:
+        for char in line:
+            summary += changeChar(char)
+
+with open("output.txt", 'w', encoding="utf8") as myFile:
+    myFile.write(summary)
