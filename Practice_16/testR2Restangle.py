66eae601fec7bfe3cc3e34055f38fb9846690999
Антон <AntaresStar86@yandex.ru>
2022-05-01 23:56:46 +0300

    New trigonometry R2

---
+++ Practice_16/testR2Restangle.py
@@ -0,0 +1,8 @@
+from restangle import Restangle
+
+r2 = Restangle(15,10)
+
+print('r2.width=', r2.width)
+print('r2.heidht=', r2.heidht)
+print('r2.getWidth=', r2.getWidth())
+print('r2.getArea', r2.getArea())
