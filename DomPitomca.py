0ccbaf1c7f4ce7d8fc720a17f50b9009078a4b56
Антон <AntaresStar86@yandex.ru>
2022-05-02 00:32:01 +0300

    Practic 16.7

--- DomPitomca.py
+++
@@ -1,5 +0,0 @@
-class Cat
-    def __init__(self, breed, gender, age):
-        self.breed = breed
-        self.gender = gender
-        self.age = age
---
+++ Practice_16/DomPitomca.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, breed, gender, age):
+        self.breed = breed
+        self.gender = gender
+        self.age = age
+
+    def getBreed(self):
+        return self.breed
+
+    def getGender(self):
+        return self.gender
+
+    def getAge(self):
+        return self.age
