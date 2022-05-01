99325de32772cce5e03b6bc6a076d2fd7d63711b
Антон <AntaresStar86@yandex.ru>
2022-05-01 23:48:48 +0300

    New trigonometry

---
+++ Practice_16/rectangle.py
@@ -0,0 +1,14 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.widht = widht
+        self.heidht = height
+
+    def getWidht(self):
+        return self.widht
+
+    def getHeidht(self):
+        return self.heidht
+
+    # Метод расчитывающий площадь
+    def getArea(self):
+        return self.widht * self.heidht
---
+++ Practice_16/testRectangle.py
@@ -0,0 +1,8 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10,5)
+
+print('r1.width=', r1.width)
+print('r1.heidht=', r1.heidth)
+print('r1.getWidht=', r1.getWidht())
+print('r1.getArea', r1.getArea())
