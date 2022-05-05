cb242bafee8183a5fc3edd58580ad3d23bcdf702
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-05 10:45:52 +0300

    задание 16.8.1

--- python_practice/rectangle_2.py
+++ python_practice/rectangle_2.py
@@ -1,7 +1,13 @@
-from rectangle import Rectangle
+from rectangle import Rectangle, Square
 
-rect_1 = Rectangle(3,4)
-rect_2 = Rectangle(12,5)
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
 
 print(rect_1.get_area())
 print(rect_2.get_area())
+
+square_1 = Square(5)
+square_2 = Square(10)
+
+print(square_1.get_area_square())
+print(square_2.get_area_square())
