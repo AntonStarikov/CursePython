da3ba1b7dc0a9fb04b129e0c9e34ca4af6b6d13e
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-05 10:18:00 +0300

    Сокращение вывода до строки

--- Practice_16/testDomPitomca.py
+++ Practice_16/testDomPitomca.py
@@ -3,9 +3,9 @@ from DomPitomca import Cat
 cat1 = Cat('Барон', 'мальчик', '2 года')
 cat2 = Cat('Сэм', 'мальчик', '2 года')
 
-print('Имя:', cat1.name)
-print('Пол:', cat1.gender)
-print('Возраст:', cat1.age)
-print('Имя:', cat2.name)
-print('Пол:', cat2.gender)
-print('Возраст:', cat2.age)
+print('Имя:', cat1.name,
+      'Пол:', cat1.gender,
+      'Возраст:', cat1.age)
+print('Имя:', cat2.name,
+      'Пол:', cat2.gender,
+      'Возраст:', cat2.age)
--- Practice_16/testRectangle.py
+++ Practice_16/testRectangle.py
@@ -6,3 +6,10 @@ print('r1.width=', r1.width)
 print('r1.heidht=', r1.heidht)
 print('r1.getWidth=', r1.getWidth())
 print('r1.getArea', r1.getArea())
+
+r2 = Rectangle(15,10)
+
+print('r2.width=', r2.width)
+print('r2.heidht=', r2.heidht)
+print('r2.getWidth=', r2.getWidth())
+print('r2.getArea', r2.getArea())
