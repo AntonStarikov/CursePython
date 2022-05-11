e2e6d2fa2b61fc9504e86198d0517fc6e0514bf2
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-11 16:29:50 +0300

    Решение практики 17.9.1

--- Modul-17.8.py
+++ Modul-17.8.py
@@ -3,12 +3,24 @@ counter = 0
 for i in range(len(array)):  # проходим по всему массиву
     idx_min = i  # сохраняем индекс предположительно минимального элемента
     for j in range(i, len(array)):
-        if array[j] < array[idx_min]:
+       if array[j] < array[idx_min]:
+            idx_min = j
             counter += 1
-            if array[j] < array[idx_min]:
-                idx_min = j
     if i != idx_min:  # если индекс не совпадает с минимальным, меняем
         array[i], array[idx_min] = array[idx_min], array[i]
 
 print(counter)
 print(array)
+
+array = [2, 3, 1, 4, 6, 5, 9, 8, 7]
+counter=0
+for i in range(1,len(array)):
+    x=array[i]
+    idx = i
+    while idx>0 and array[idx-1]>x:
+        counter += 1
+        array[idx] = array[idx-1]
+        idx-=1
+    array[idx]=x
+print(array)
+print(counter)
