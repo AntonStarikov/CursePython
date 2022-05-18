67772438d350a84a7593dd5c44fcbd58adefa725
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-18 14:37:46 +0300

    Практикум 18 написание телебота

--- Practice_18/HomeWoork-18.2.py
+++ Practice_18/HomeWoork-18.2.py
@@ -1,11 +1,16 @@
 import telebot
+from telebot import TeleBot
 
 TOKEN = "5313606339:AAGQK-c56x13G3bA-nJzDr-WXNkRjXuW-F4"
 
-bot = telebot.TeleBot(TOKEN)
+bot: TeleBot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(filters)
-def function_name(message):
-    bot.reply_to(message, "This is a message handler")
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f"Приветствую, {message.chat.username}")
+
+@bot.message_handler(content_types=['photo',])
+def massage_photo(message: telebot.types.Message):
+    bot.reply_to(message,'Nice meme XDD')
 
 bot.polling(none_stop=True)
