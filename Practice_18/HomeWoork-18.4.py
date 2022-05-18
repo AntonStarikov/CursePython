cd31dadebdecfb84121c763c0006363fcc64d20c
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-18 22:48:42 +0300

    Практикум 18.5

--- Practice_18/HomeWoork-18.4.py
+++ Practice_18/HomeWoork-18.4.py
@@ -3,8 +3,9 @@ import lxml.html
 from lxml import etree
 
 tree = etree.parse('Welcome to Python.org.html', lxml.html.HTMLParser())
-ul = tree.findall('//*[@id="content"]/div/section/div[3]/div[1]/div/ul/li[3]')
+ul = tree.findall('//*[@id="content"]/div/section/div[3]/div[1]/div/ul/li')
 
 for li in ul:
     a = li.find('a')
-    print(a.text)
+    time = li.find('time')
+    print(time.get('datetime'), a.text)
