ca6c75b580b0b75de6d1e225045e2c831f7fe7db
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-19 15:27:45 +0300

    HomeWoork redis

--- Practice_18/Redis_18.5.py
+++ Practice_18/Redis_18.5.py
@@ -1,6 +1,30 @@
 import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
-r.set('foo', 'bar')
-True
-r.get('foo')
-b'bar'
+import json
+
+red = redis.Redis(
+    host='redis-16456.c1.asia-northeast1-1.gce.cloud.redislabs.com',
+    port=16456,
+    password='5ysiDjs2cgiXa3bKLDA8dclvoCwhkSfm'
+)
+cont = True
+
+while cont:
+    action = input('action:\t')
+    if action == 'write':
+        name = input('name:\t')
+        phone = input('phone:\t')
+        red.set(name, phone)
+    elif action == 'read':
+        name = input('name:\t')
+        phone = red.get(name)
+        if phone:
+            print(f'{name}\'s phone is {str(phone)}')
+    elif action == 'delete':
+        name = input('name:\t')
+        phone = red.delete(name)
+        if phone:
+            print(f"{name}'s phone is deleted")
+        else:
+            print(f"Not found {name}")
+    elif action == 'stop':
+        break
