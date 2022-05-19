f350d124d93ec07e997041a391d98c8b82a80a99
Dywolenok091086 <AntaresStar86@yandex.ru>
2022-05-19 15:58:04 +0300

    Cripto_bot final

--- Cripto_Bot/app.py
+++ Cripto_Bot/app.py
@@ -1,15 +1,15 @@
 import telebot
 from config import  keys, TOKEN
-from utlity import ConvertionException, CriptoConverter
+from extensions import ConvertionException, CriptoConverter
 
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message: telebot.types.Message):
-    text = 'Чтобы начать работу, введите команду боту в следующем формате:\n<имя валюты> \
-<в какую валюту перевести> \
-<количество переводимой валюты>\nУвидить список всех доступных валют: /values'
+    text = 'Чтобы начать работу, введите команду боту в следующем формате:\n<имя валюты цену которой хотите узнать> \
+<имя валюты в которой надо узнать цену первой валюты> \
+<количество первой валюты>\nУвидить список всех доступных валют: /values'
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['values'])
@@ -21,15 +21,22 @@ def valued(message: telebot.types.Message):
 
 @bot.message_handler(content_types=['text', ])
 def convert(message: telebot.types.Message):
-    values = message.text.split(' ')
+    try:
+        values = message.text.split(' ')
 
-    if len(values) != 3:
-        raise ConvertionException('Слишком много параметров.')
+        if len(values) != 3:
+           raise ConvertionException('Слишком много параметров.')
 
-    quote, base, amount = values
-    total_base = CriptoConverter.convert(quote, base, amount)
+        quote, base, amount = values
+        total_base = CriptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+         bot.reply_to(message, f'Ощибка пользователя!\n{e}')
 
-    text = f'Цена {amount} {quote} в {base} = {total_base}'
-    bot.send_message(message.chat.id, text)
+    except Exception as e:
+         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+
+    else:
+         text = f'Цена {amount} {quote} в {base} = {total_base}'
+         bot.send_message(message.chat.id, text)
 
 bot.polling()
--- Cripto_Bot/config.py
+++ Cripto_Bot/config.py
@@ -1,8 +1,7 @@
 TOKEN = "5382817722:AAFAHHnwpJ3qcJW08Sblt0CG_JodXoWKfTo"
 
 keys = {
-    'биткоин': 'BTC',
-    'эфириум': 'ETH',
+    'евро': 'EUR',
     'доллар': 'USD',
     'рубль': 'RUB',
 }
